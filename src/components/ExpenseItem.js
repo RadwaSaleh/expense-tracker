95c97698a39623f506724b45d36ec9ad547aeb9a
Radwa Saleh <33086179+RadwaSaleh@users.noreply.github.com>
2021-11-16 13:00:55 +0100

    splitting date formatting in a separate component "ExpenseDate" (#1)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     return(
         <div className="expense-item">
-            <div>{props.date.toLocaleDateString("en-US", options)}</div>
+            <ExpenseDate date={props.date} className="expense-date"/>
             <div className="expense-item__description">
                 <h2 className="expense-item h2">{props.title}</h2>
                 <div className="expense-item__price">{props.amount} EUR</div>
