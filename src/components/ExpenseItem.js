51107ccd1f5356bb563b55c77ce5a012764a7d42
Radwa Saleh <>
2022-09-29 16:26:14 +0200

    Add expenses year filter Remove change title button and convert ExpenseItem component to stateless component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,23 +1,15 @@
-import React, {useState} from "react";
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 
+// Stateless component = presentational component = dumb component
 function ExpenseItem(props){
-    const [title, setTitle] = useState(props.title)
-    const clickHandler = () => {
-        let userInput = prompt('Please Enter your Name');
-        if(userInput != null){
-            setTitle(userInput);
-        }
-    }
     return(
         <div className="expense-item">
             <ExpenseDate date={props.date} className="expense-date"/>
             <div className="expense-item__description">
-                <h2 className="expense-item h2">{title}</h2>
+                <h2 className="expense-item h2">{props.title}</h2>
                 <div className="expense-item__price">{props.amount} EUR</div>
             </div>
-            <button className="expense-item__change_btn" onClick={clickHandler}>Change Title</button>
         </div>
     );
 }
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,21 @@
+import {useState} from "react";
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
     const items = [];
+    const [expensesYear, setExpensesYear] = useState('2022');
+    const selectExpensesYearHandler = (selectedExpensesYear) => {
+        setExpensesYear(selectedExpensesYear);
+    }
+
     for(let i=0; i < props.items.length; i++){
         items.push(<ExpenseItem title={props.items[i].title} amount={props.items[i].amount} date={props.items[i].date}/>)
     }
     return(
         <div className="expenses">
+            <ExpensesFilter selectedOption={expensesYear} onSelectExpensesYear={selectExpensesYearHandler}/>
             {items}
         </div>
     );
