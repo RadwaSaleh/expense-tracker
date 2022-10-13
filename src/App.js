029cd20347698036b0a0464f0935ffbdd41a4770
Radwa Saleh <salehradwa@gmail.com>
2022-10-13 12:57:26 +0200

    1- Remove change title button and make ExpenseItem a stateless component 2- render expense items by mapping FIXED_EXPENSES array to ExpenseItem 3- Complete add new item on top feature

--- src/App.js
+++ src/App.js
@@ -1,37 +1,40 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
 import Expenses from './components/Expenses';
 import NewExpense from "./components/NewExpense";
 
+const FIXED_EXPENSES = [
+    {
+        id: Math.random(),
+        title: 'Toilet Paper',
+        amount: 94.12,
+        date: new Date(2020, 7, 14),
+    },
+    {
+        id: Math.random(),
+        title: 'New TV',
+        amount: 799.49,
+        date: new Date(2021, 2, 12) },
+    {
+        id: Math.random(),
+        title: 'Car Insurance',
+        amount: 294.67,
+        date: new Date(2021, 2, 28),
+    },
+    {
+        id: Math.random(),
+        title: 'New Desk (Wooden)',
+        amount: 450,
+        date: new Date(2021, 5, 12),
+    },
+];
 function App() {
-    const expenses = [
-        {
-            id: 'e1',
-            title: 'Toilet Paper',
-            amount: 94.12,
-            date: new Date(2020, 7, 14),
-        },
-        {
-            id: 'e2',
-            title: 'New TV',
-            amount: 799.49,
-            date: new Date(2021, 2, 12) },
-        {
-            id: 'e3',
-            title: 'Car Insurance',
-            amount: 294.67,
-            date: new Date(2021, 2, 28),
-        },
-        {
-            id: 'e4',
-            title: 'New Desk (Wooden)',
-            amount: 450,
-            date: new Date(2021, 5, 12),
-        },
-    ];
+    const [expenses, setExpenses] = useState(FIXED_EXPENSES);
 
-    const addNewExpenseHandler = (newExpense) => {
-        expenses.push(newExpense)
+    const addNewExpenseHandler = (expense) => {
+        setExpenses((prevState) => {
+            return [expense, ...prevState];
+        });
     }
   return (
     <div className="App">
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,19 +4,15 @@ import './Expenses.css';
 import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props){
-    const items = [];
     const [expensesYear, setExpensesYear] = useState('2022');
     const selectExpensesYearHandler = (selectedExpensesYear) => {
         setExpensesYear(selectedExpensesYear);
     }
 
-    for(let i=0; i < props.items.length; i++){
-        items.push(<ExpenseItem title={props.items[i].title} amount={props.items[i].amount} date={props.items[i].date}/>)
-    }
     return(
         <div className="expenses">
             <ExpensesFilter selectedOption={expensesYear} onSelectExpensesYear={selectExpensesYearHandler}/>
-            {items}
+            {props.items.map((item) => <ExpenseItem title={item.title} amount={item.amount} date={item.date}/>)}
         </div>
     );
 }
