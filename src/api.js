daaa7474f814245d258be62482a662c393afc7a3
Radwa Shehata <radwa.shehata@justeattakeaway.com>
2023-10-27 11:03:48 +0200

    latest changes

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+const axios = require('axios');
+const MockAdapter = require('axios-mock-adapter');
 const mock = new MockAdapter(axios);
 
 mock.onGet("http://localhost:3000/expenses")
